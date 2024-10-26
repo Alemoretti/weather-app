c88e1218d20c078b6e86a8b9154e4097e4e1ec4a
Alemoretti <alemoretti10@gmail.com>
2024-10-26 07:52:44 -0300

    Change Location component name to follow multi word components naming

--- resources/js/Components/WeatherOverview.vue
+++ resources/js/Components/WeatherOverview.vue
@@ -1,6 +1,6 @@
 <script setup>
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import Location from '@/Components/Location/Location.vue';
+import Location from '@/Components/Location/LocationWeather.vue';
 </script>
 
 <template>
@@ -10,21 +10,42 @@ import Location from '@/Components/Location/Location.vue';
       <form class="mt-4">
         <div class="flex space-x-4">
           <div class="flex-1">
-            <label for="city" class="block text-sm font-medium">City</label>
-            <input type="text" id="city" name="city" class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm" />
+            <label
+              for="city"
+              class="block text-sm font-medium"
+            >City</label>
+            <input
+              type="text"
+              id="city"
+              name="city"
+              class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm"
+            >
           </div>
           <div class="flex-1">
             <label 
               for="state" 
-              class="block text-sm font-medium">
+              class="block text-sm font-medium"
+            >
               State
             </label>
-            <input type="text" id="state" name="state" class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm" />
+            <input
+              type="text"
+              id="state"
+              name="state"
+              class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm"
+            >
           </div>
         </div>
         <div class="mt-4 flex items-center space-x-2">
-          <button type="submit" class="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700">Add</button>
-          <p class="text-sm text-gray-500">You can save up to 3 records</p>
+          <button
+            type="submit"
+            class="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700"
+          >
+            Add
+          </button>
+          <p class="text-sm text-gray-500">
+            You can save up to 3 records
+          </p>
         </div>
       </form>
     </div>
