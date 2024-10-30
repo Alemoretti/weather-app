99f388417c0ed4dd1190df244effbc354da14b15
Alemoretti <alemoretti10@gmail.com>
2024-10-30 14:17:42 -0300

    Get LocationRequest validation errors and use state to show them

--- resources/js/Store/LocationStore.js
+++ resources/js/Store/LocationStore.js
@@ -1,39 +1,47 @@
-import { defineStore } from 'pinia';
-import axios from 'axios';
+import { defineStore } from 'pinia'
+import axios from 'axios'
 
 export const useLocationStore = defineStore('location', {
   state: () => ({
-    locations: []
+    locations: [],
+    errors: {}
   }),
   actions: {
     async fetchLocations() {
       try {
-        const response = await axios.get('/api/locations');
-        this.locations = response.data;
+        const response = await axios.get('/api/locations')
+        this.locations = response.data
       } catch (error) {
-        console.error('Error fetching locations:', error);
+        console.error('Error fetching locations:', error)
       }
     },
     async addLocation(locationData) {
         try {
-            const response = await axios.post('/api/locations', locationData);
-            this.locations.push(response.data);
+          const response = await axios.post('/api/locations', locationData)
+          this.locations.push(response.data)
+          this.errors = {}
         } catch (error) {
-            console.error('Error adding location:', error);
+            console.log(error.response.data)
+          if (error.response && error.response.status === 422) {
+            this.errors = error.response.data.errors
+          } else {
+            console.error('Error adding location:', error)
+          }
         }
-    },
+      },
     async deleteLocation(locationId) {
         try {
-            await axios.delete(`/api/locations/${locationId}`);
-            this.locations = this.locations.filter(location => location.id !== locationId);
+            await axios.delete(`/api/locations/${locationId}`)
+            this.locations = this.locations.filter(location => location.id !== locationId)
         } catch (error) {
-            console.error('Error deleting location:', error);
+            console.error('Error deleting location:', error)
         }
     },
   },
   getters: {
     getLocations: (state) => state.locations,
     getLocationById: (state) => (id) => state.locations.find(location => location.id === id),
-    getLocationCount: (state) => state.locations.length
+    getLocationCount: (state) => state.locations.length,
+    getErrors: (state) => state.errors
   }
-});
+})
