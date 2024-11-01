aa7321968cad9add1f32423028394a35f05417f3
Alemoretti <alemoretti10@gmail.com>
2024-11-01 09:32:09 -0300

    Change Laravel's welcome page

---
+++ resources/js/Pages/HomePage/HomePage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="min-h-screen flex flex-col justify-center items-center bg-gray-100">
+    <ApplicationLogo />
+    <h1 class="mt-4 text-2xl font-bold text-indigo-700">
+      Weather App
+    </h1>
+    <div class="mt-6">
+      <a
+        href="/login"
+        class="text-lg font-semibold text-gray-700 hover:text-gray-900"
+      >
+        Login
+      </a>
+      <a
+        href="/register"
+        class="ml-4 text-lg font-semibold text-gray-700 hover:text-gray-900"
+      >
+        Register
+      </a>
+    </div>
+  </div>
+</template>
+  
+  <script setup lang="ts">
+  import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+  </script>
